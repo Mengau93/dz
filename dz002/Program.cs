51b419291ae4576baad5f44b55fb623e78e9ff08
Roman <android91@mail.ru>
2022-06-15 20:11:21 +0300

    add dz 002

---
+++ dz002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = 4;
+int b = -6;
+int c = -12;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write ("max = ");
+Console.WriteLine(max);
