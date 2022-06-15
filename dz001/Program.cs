196a45901857b8a0a7a06b8a6a93b49b9e2a2abe
Roman <android91@mail.ru>
2022-06-15 19:39:19 +0300

    add dz 001

---
+++ dz001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = 12;
+int b = 10;
+
+int max = a;
+int min = b;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write ("max = ");
+Console.WriteLine(max);
+
+Console.Write ("min = ");
+Console.WriteLine(min);
